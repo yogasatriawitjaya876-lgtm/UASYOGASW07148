56d09150e0eebccad3f691ff9329bb1eb6c5d5dd
(Gen)Yoga Satria Witjaya <yogasatriawitjaya876@gmail.com>
2026-01-16 22:05:44 +0700

    Delete genspeedshop directory

--- genspeedshop/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "koneksi.php";
-
-$id = $_GET['id'];
-
-mysqli_query($koneksi, "DELETE FROM pesanan WHERE id='$id'");
-
-header("Location: keranjang.php");
-exit;
--- genspeedshop/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "genspeedshop");
-
-if (!$koneksi) {
-    die("Koneksi database gagal");
-}
